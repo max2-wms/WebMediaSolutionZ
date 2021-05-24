d6c9d4ac092294f1345cc3c5d6abd50b98a2e889
Maxime Pierre <max@webmediasolutionz.com>
2021-05-24 13:07:56 -0400

    adding deployement bash script

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=(date -I)
+
+# Building image
+docker build -t max2wms/wms:{$DATE} --no-cache .
+docker tag max2wms/wms:{$DATE} max2wms/wms
+
+# Push image
+docker push max2wms/wms:{$DATE}
+docker push max2wms/wms
+
+# Redeploy stack
+npm run swarm:down
+
+sleep 20
+
+npm run swarm:up
