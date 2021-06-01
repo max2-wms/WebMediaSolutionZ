8ede71f3563e93c667861bfc1ae97303573b6ce6
Maxime Pierre <max@webmediasolutionz.com>
2021-05-31 20:30:29 -0400

    tweaking tooling and deployement script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DATE=(date -I)
+DATE=$(date +'%s')
 
 # Building image
-docker build -t max2wms/wms:{$DATE} --no-cache .
-docker tag max2wms/wms:{$DATE} max2wms/wms
+docker build -t max2wms/wms:$DATE --no-cache .
+docker tag max2wms/wms:$DATE max2wms/wms
 
 # Push image
-docker push max2wms/wms:{$DATE}
+docker push max2wms/wms:$DATE
 docker push max2wms/wms
 
 # Redeploy stack
